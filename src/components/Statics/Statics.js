34ffb7c7ced5080da029bfde4c3afa0755c185ab
masumbillah360 <masumbillah6584@gmail.com>
2022-10-12 02:08:25 +0600

    fix charts issue

--- src/components/Statics/Statics.js
+++ src/components/Statics/Statics.js
@@ -4,14 +4,10 @@ import { QuizContext } from "../Layout/Layout";
 
 const Statics = () => {
 	const data = useContext(QuizContext);
-	// console.log(lineData.data);
-	// const data = [{ name: "Page A", uv: 400, pv: 2400, amt: 2400 }];
 	return (
-		<div className="min-h-screen min-w-full flex justify-center items-center">
-			<div className="w-full md:w-2/3 bg-slate-100">
-				<h1 className="text-center font-semibold">
-					Quiz Questions Chart
-				</h1>
+		<div className="min-h-screen min-w-full flex flex-col justify-center items-center">
+			<div className="w-full md:w-2/3 bg-slate-100 p-2 rounded">
+				<h1 className="text-center font-semibold">Quiz Questions Chart</h1>
                 <ResponsiveContainer height={300} width='100%'>
 					<BarChart width={900} height={300} data={data.data}>
 						<XAxis dataKey="name" stroke="#8884d8" />

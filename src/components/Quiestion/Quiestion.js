fd52bad7c5f0a88887d1f8df71320b7b83b77f96
masumbillah360 <masumbillah6584@gmail.com>
2022-10-12 11:52:14 +0600

    set conditional rendering on option selection

--- src/components/Quiestion/Quiestion.js
+++ src/components/Quiestion/Quiestion.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaEye } from "react-icons/fa";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -6,21 +6,25 @@ import "react-toastify/dist/ReactToastify.css";
 const Quiestion = ({ questions }) => {
 	const { options, question, correctAnswer } = questions;
 	const plainQuestion = question.split("<p>").join("").split("</p>").join("");
+	const [checked, setChecked] = useState(undefined);
 
 	const answerDetector = (option) => {
 		if (option === correctAnswer) {
 			toast.success("correct", correctAnswer);
+			setChecked(true);
 		} else {
 			toast.warning("wrong answer");
+			setChecked(false);
 		}
 	};
+
 	const showAnser = (ans) => {
 		toast.info(ans);
 	};
 	return (
 		<div className="border my-2 p-2">
 			<div className="flex justify-between">
-				<label className="font-bold py-1 mb-1" htmlFor='qsn'>
+				<label className="font-bold py-1 mb-1" htmlFor="qsn">
 					Q: {plainQuestion}
 				</label>
 				<button
@@ -34,17 +38,24 @@ const Quiestion = ({ questions }) => {
 			</div>
 			<ul className="grid md:grid-cols-2 gap-2">
 				{options.map((option, idx) => (
-					<li className="bg-slate-100 p-2 rounded hover:bg-green-100 form-control" key={idx}>
-							<label className="label cursor-pointer">
-								<span className="label-text">{option}</span>
-								<input
-									type="radio"
-									onClick={() => answerDetector(option)}
-									name='qsn'
-									className="radio checked:bg-blue-500"
+					<li
+						className="bg-slate-100 p-2 rounded hover:bg-green-100 form-control"
+						key={idx}
+					>
+						<label className="label cursor-pointer">
+							<span className="label-text">{option}</span>
+							<input
+								type="radio"
+								onClick={() => answerDetector(option)}
+								name="qsn"
+								className={`radio ${
 									checked
-								/>
-							</label>
+										? "checked:bg-green-900"
+										: "checked:bg-red-700"
+								} `}
+								defaultChecked
+							/>
+						</label>
 					</li>
 				))}
 			</ul>

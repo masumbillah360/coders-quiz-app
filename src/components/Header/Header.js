6f6434118f01dc04c5073148406a56417dab4d91
masumbillah360 <masumbillah6584@gmail.com>
2022-10-12 15:48:54 +0600

    added photo on header components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import './Header.css'
 const Header = () => {
     return (
-        <div className='p-8 flex flex-col bg-slate-200 text-center rounded'>
-            <h1 className='text-3xl font-bold text-info'>Coder's Quiz App</h1>
-            <p className='mt-2'>This is simple Quiz App for <span className='text-info'>New</span> Coder's. It's helpfull for new coder's how to solve a quize.</p>
+        <div className='p-8 flex flex-col text-center rounded bg-image'>
+            <img className='w-28 mx-auto' src="header-photo.png" alt="" />
+            <h1 className='text-3xl font-bold text-white'>Coder's Quiz App</h1>
+            <p className='mt-2 text-white'>This is simple Quiz App for <span className='text-info'>New</span> Coder's. It's helpfull for new coder's how to solve a quize.</p>
         </div>
     );
 };

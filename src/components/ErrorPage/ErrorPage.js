a04dd74dbac8621a287b5bf00e28d22f9b653b4f
masumbillah360 <masumbillah6584@gmail.com>
2022-10-12 21:35:20 +0600

    fix error page issue

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -1,13 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
+	const error = useRouteError();
 	return (
 		<div className="min-h-screen flex items-center justify-center">
 			<div className="text-center text-slate-500">
-				<h1 className="text-7xl font-bold">404</h1>
-				<h2 className="text-9xl font-bold">Page Not Found</h2>
-				<Link className="btn btn-warning mt-3 font-bold" to='/home'>Back to Home</Link>
+				<br />
+				{error && (
+					<div>
+						<h2 className="text-4xl font-bold">Opps!!</h2>
+						<h1 className="text-3xl font-bold">{error.status}</h1>
+						<p className="text-4xl font-bold text-red-600">{error.statusText || error.message}</p>
+						<Link
+							className="btn btn-sm btn-warning mt-3 font-bold"
+							to="/home"
+						>
+							Back to Home
+						</Link>
+					</div>
+				)}
 			</div>
 		</div>
 	);
